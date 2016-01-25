594c95409ed166540e7fd6e42a9d09745ff02cac
Mike Naberezny <mike@naberezny.com>
2016-01-25 13:13:56 -0800

    Add clear() method to clear the display

--- lcd_control.py
+++ lcd_control.py
@@ -10,7 +10,7 @@ class Lcd(object):
 
     def spi(self, data):
         self.device.getFeedback(u3.BitStateWrite(IONumber=u3.FIO0, State=1))
-        result = self.device.spi(
+        data = self.device.spi(
             SPIBytes=data,
             AutoCS=False,
             DisableDirConfig=False,
@@ -20,16 +20,21 @@ class Lcd(object):
             MOSIPinNum=u3.FIO1,
             CLKPinNum=u3.FIO2,
             MISOPinNum=u3.FIO3,
-            )
-        data = result['SPIBytes']
+            )['SPIBytes']
         self.device.getFeedback(u3.BitStateWrite(IONumber=u3.FIO0, State=0))
         self.device.getFeedback(u3.BitStateWrite(IONumber=u3.FIO1, State=0))
         return data
 
+    def clear(self):
+        self.spi([0x04]) # Display Setting command
+        self.spi([0xcf]) # Status command
+        self.spi([0x41]) # Data Setting command: write to pictograph ram
+        self.spi([0x80] + ([0]*8)) # Address Setting command, pictograph data
+        self.spi([0x40]) # Data Setting command: write to display ram
+        self.spi([0x80] + ([0]*16)) # Address Setting command, display data
+
     def write(self, text):
-        self.spi([0x04]) # Display Setting Command
-        self.spi([0xcf]) # Status Command
-        self.spi([0x40]) # Data Setting Command: write to display ram
+        self.spi([0x40]) # Data Setting command: write to display ram
         text = reversed(text[:11].ljust(11, ' '))
         packet = [0x82] + list([ self.char_code(c) for c in text ])
         self.spi(packet)
@@ -39,18 +44,14 @@ class Lcd(object):
             raise ValueError("Character number %r is not 0-15", index)
         if len(data) != 7:
             raise ValueError("Character data length %r is not 7" % len(data))
-        self.spi([0x4a]) # Data Setting Command: write to chargen ram
-        packet = [0x80 + index] + list(data) # Address Setting Command, data
+        self.spi([0x4a]) # Data Setting command: write to chargen ram
+        packet = [0x80 + index] + list(data) # Address Setting command, data
         self.spi(packet)
 
     def display_charset(self):
-        self.spi([0x04]) # Display Setting Command
-        self.spi([0xcf]) # Status Command
-        self.spi([0x41]) # Data Setting Command: write to pictograph ram
-        self.spi([0x80] + ([0]*8)) # Address Setting command, pictograph data
-        self.spi([0x40]) # Data Setting Command: write to display ram
+        self.spi([0x40]) # Data Setting command: write to display ram
         for i in range(0, 0x100, 7):
-            data = [ ord(c) for c in '0x%02X' % i ]
+            data = [ ord(c) for c in '0x%02X' % i ] # 4 chars: code in hex
             for j in range(7): # 7 characters
                 code = i + j
                 if code > 0xFF: # past end of charset
@@ -60,7 +61,7 @@ class Lcd(object):
 
             self.spi([0x82] + data) # Address Setting command, display data
             time.sleep(1)
-        self.write('')
+        self.clear()
 
     def display_rom_charset_comparison(self):
         def read_char_data(charset, index):
@@ -73,7 +74,7 @@ class Lcd(object):
             data = read_char_data(lcd_charsets.PREMIUM_4, i)
             lcd.define_char(0, data)
 
-            # first four chars: display character code in hx
+            # first four chars: display character code in hex
             lcd.spi([0x40])
             data = list(reversed([ ord(c) for c in '0x%02X' % i ]))
             lcd.spi([0x89] + data)
@@ -83,6 +84,7 @@ class Lcd(object):
                 for code in (0, i):
                     lcd.spi([0x82] + ([code]*7))
                     time.sleep(0.2)
+        self.clear()
 
     CHARACTERS = lcd_decode.Premium4.CHARACTERS
     def char_code(self, char):
@@ -99,12 +101,8 @@ if __name__ == '__main__':
     device = u3.U3()
     try:
         lcd = Lcd(device)
-
-        lcd.spi([0x04]) # Display Setting command
-        lcd.spi([0xcf]) # Status command
-
+        lcd.clear()
         lcd.display_rom_charset_comparison()
-
 
     finally:
         device.getFeedback(u3.BitStateWrite(IONumber=u3.FIO0, State=0)) # STB
