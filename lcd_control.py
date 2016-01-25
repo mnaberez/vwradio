08213e4c2dcd0d7650fb159912bd2423b3f60d61
Mike Naberezny <mike@naberezny.com>
2016-01-25 15:10:18 -0800

    Extract demos into Demonstrator class

--- lcd_control.py
+++ lcd_control.py
@@ -54,20 +54,34 @@ class Lcd(object):
         packet = [0x80 + index] + list(data) # Address Setting command, data
         self.spi(packet)
 
-    def display_charset(self):
+    CHARACTERS = lcd_decode.Premium4.CHARACTERS
+    def char_code(self, char):
+        if char in string.digits:
+            return ord(char)
+        for key, value in self.CHARACTERS.items():
+            if value == char:
+                return key
+        return 0x7f # not found, show block cursor char
+
+
+class Demonstrator(object):
+    def __init__(self, lcd):
+        self.lcd = lcd
+
+    def show_charset(self):
         for i in range(0, 0x100, 7):
-            self.write('0x%02X' % i, pos=0)
+            self.lcd.write('0x%02X' % i, pos=0)
             data = [0x20] * 7 # 7 chars, default blank spaces
             for j in range(len(data)):
                 code = i + j
                 if code > 0xFF: # past end of charset
                     break
                 data[j] = code
-            self.write_codes(data, pos=4)
+            self.lcd.write_codes(data, pos=4)
             time.sleep(1)
-        self.clear()
+        self.lcd.clear()
 
-    def display_rom_charset_comparison(self):
+    def show_rom_charset_comparison(self):
         def read_char_data(charset, index):
             start = i * 7
             end = start + 7
@@ -79,31 +93,31 @@ class Lcd(object):
             lcd.define_char(0, data)
 
             # first four chars: display character code in hex
-            self.write('0x%02X' % i, pos=0)
+            self.lcd.write('0x%02X' % i, pos=0)
 
             # all other chars: alternate between rom and programmable char 0
             for blink in range(6):
                 for code in (0, i):
-                    self.write_codes([code]*7, pos=4)
+                    self.lcd.write_codes([code]*7, pos=4)
                     time.sleep(0.2)
-        self.clear()
+        self.lcd.clear()
 
-    CHARACTERS = lcd_decode.Premium4.CHARACTERS
-    def char_code(self, char):
-        if char in string.digits:
-            return ord(char)
-        for key, value in self.CHARACTERS.items():
-            if value == char:
-                return key
-        return 0x7f # not found, show block cursor char
+    def clock(self):
+        lcd.write("Time", pos=0)
+        while True:
+            clock = time.strftime("%I:%M%p").lower()
+            if clock[0] == '0':
+                clock = ' ' + clock[1:]
+            lcd.write(clock, pos=4)
+            time.sleep(0.5)        
 
 
 if __name__ == '__main__':
     device = u3.U3()
     try:
         lcd = Lcd(device)
-        lcd.clear()
-        lcd.display_rom_charset_comparison()
+        demo = Demonstrator(lcd)
+        demo.show_rom_charset_comparison()
 
     finally:
         device.getFeedback(u3.BitStateWrite(IONumber=u3.FIO0, State=0)) # STB
