0319909dc82e2b922439bf0f83521f3dccf5bbe6
Mike Naberezny <mike@naberezny.com>
2016-01-31 15:09:14 -0800

    Add Sony MDX-C7900R charset dumped by Kęstutis Bivainis. The Sony faceplate has no relation to the VW radios made by Clarion but uses the same LCD controller (NEC uPD16432B).

--- vwradio/charsets.py
+++ vwradio/charsets.py
@@ -1,4 +1,4 @@
-_PREMIUM_4 = '''
+_VW_PREMIUM_4 = '''
     0x00:  0x01:  0x02:  0x03:  0x04:  0x05:  0x06:  0x07:
     .....  .....  .....  .....  .....  .....  .....  .....
     .....  .....  .....  .....  .....  .....  .....  .....
@@ -288,7 +288,7 @@ _PREMIUM_4 = '''
     .....  .....  .....  .....  .....  .....  .....  .....
 '''
 
-_PREMIUM_5 = '''
+_VW_PREMIUM_5 = '''
     0x00:  0x01:  0x02:  0x03:  0x04:  0x05:  0x06:  0x07:
     .....  .....  .....  .....  .....  .....  .....  .....
     .....  .....  .....  .....  .....  .....  .....  .....
@@ -578,6 +578,296 @@ _PREMIUM_5 = '''
     O....  ...OO  .O...  .OOOO  OOOO.  OOOOO  ...O.  .....
 '''
 
+_SONY_MDX_C7900R = '''
+    0x00:  0x01:  0x02:  0x03:  0x04:  0x05:  0x06:  0x07:
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+
+    0x08:  0x09:  0x0A:  0x0B:  0x0C:  0x0D:  0x0E:  0x0F:
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+    .....  .....  .....  .....  .....  .....  .....  .....
+
+    0x10:  0x11:  0x12:  0x13:  0x14:  0x15:  0x16:  0x17:
+    .....  .....  .....  .....  ...OO  OO...  ...OO  OO...
+    .....  OO.OO  ...OO  OO...  ...OO  OO...  OO.OO  OO.OO
+    .....  OO.OO  ...OO  OO...  ...OO  OO...  OO.OO  OO.OO
+    OO.OO  OO.OO  OO.OO  OO.OO  OO.OO  OO.OO  OO.OO  OO.OO
+    .....  OO.OO  ...OO  OO...  ...OO  OO...  OO.OO  OO.OO
+    .....  OO.OO  ...OO  OO...  ...OO  OO...  OO.OO  OO.OO
+    .....  .....  .....  .....  ...OO  OO...  ...OO  OO...
+
+    0x18:  0x19:  0x1A:  0x1B:  0x1C:  0x1D:  0x1E:  0x1F:
+    .....  .....  .....  .....  .....  OOOOO  O.OOO  O..O.
+    OOOOO  OOOOO  ....O  O....  .....  ....O  O.O.O  O..O.
+    OOOOO  OO.OO  ..OOO  OOO..  .....  ....O  O.O.O  O..O.
+    O...O  O...O  OOOOO  OOOOO  .....  .....  O.O.O  O..O.
+    OOOOO  OO.OO  ..OOO  OOO..  O....  .....  O.O.O  O..O.
+    OOOOO  OOOOO  ....O  O....  O....  .....  O.O.O  O..O.
+    .....  .....  .....  .....  OOOOO  .....  O.OOO  O..O.
+
+    0x20:  0x21:  0x22:  0x23:  0x24:  0x25:  0x26:  0x27:
+    .....  ..O..  OO.OO  .O.O.  ..O..  OO...  .O...  .OO..
+    .....  ..O..  .O..O  OOOOO  .OOOO  OO..O  O.O..  ..O..
+    .....  ..O..  O..O.  .O.O.  O.O..  ...O.  O.O.O  .O...
+    .....  ..O..  .....  .O.O.  .OOO.  ..O..  .O..O  .....
+    .....  ..O..  .....  OOOOO  ..O.O  .O...  O.O..  .....
+    .....  .....  .....  .O.O.  OOOO.  O..OO  O..O.  .....
+    .....  ..O..  .....  .O.O.  ..O..  ...OO  .OO.O  .....
+
+    0x28:  0x29:  0x2A:  0x2B:  0x2C:  0x2D:  0x2E:  0x2F:
+    ...O.  .O...  .....  .....  .....  .....  .....  .....
+    ..O..  ..O..  ..O..  ..O..  .....  .....  .....  ....O
+    .O...  ...O.  O.O.O  ..O..  .....  .....  .....  ...O.
+    .O...  ...O.  .OOO.  OOOOO  .....  OOOOO  .....  ..O..
+    .O...  ...O.  O.O.O  ..O..  .OO..  .....  .....  .O...
+    ..O..  ..O..  ..O..  ..O..  ..O..  .....  .OO..  O....
+    ...O.  .O...  .....  .....  .O...  .....  .OO..  .....
+
+    0x30:  0x31:  0x32:  0x33:  0x34:  0x35:  0x36:  0x37:
+    .OOO.  ...O.  .OOO.  .OOO.  ...O.  OOOOO  ..OO.  OOOOO
+    O...O  .OOO.  O...O  O...O  ..OO.  O....  .O...  ....O
+    O...O  ...O.  ....O  ....O  .O.O.  OOOO.  O....  ...O.
+    O...O  ...O.  ..OO.  ..OO.  O..O.  ....O  OOOO.  ...O.
+    O...O  ...O.  .O...  ....O  OOOOO  ....O  O...O  ..O..
+    O...O  ...O.  O....  O...O  ...O.  O...O  O...O  ..O..
+    .OOO.  ...O.  OOOOO  .OOO.  ...O.  .OOO.  .OOO.  ..O..
+
+    0x38:  0x39:  0x3A:  0x3B:  0x3C:  0x3D:  0x3E:  0x3F:
+    .OOO.  .OOO.  .....  .....  ....O  .....  O....  .OOO.
+    O...O  O...O  .OO..  .OO..  ...O.  .....  .O...  O...O
+    O...O  O...O  .....  .....  ..O..  OOOOO  ..O..  ....O
+    .OOO.  .OOOO  .....  .....  .O...  .....  ...O.  ...O.
+    O...O  ....O  .....  .OO..  ..O..  OOOOO  ..O..  ..O..
+    O...O  ...O.  .OO..  ..O..  ...O.  .....  .O...  .....
+    .OOO.  .OO..  .....  .O...  ....O  .....  O....  ..O..
+
+    0x40:  0x41:  0x42:  0x43:  0x44:  0x45:  0x46:  0x47:
+    .OOO.  ..O..  OOOO.  .OOO.  OOOO.  OOOOO  OOOOO  .OOO.
+    O...O  .O.O.  .O..O  O...O  .O..O  O....  O....  O...O
+    O..OO  O...O  .O..O  O....  .O..O  O....  O....  O....
+    O.O.O  O...O  OOOO.  O....  .O..O  OOOO.  OOOO.  O.OOO
+    O.O.O  OOOOO  .O..O  O....  .O..O  O....  O....  O...O
+    O..OO  O...O  .O..O  O...O  .O..O  O....  O....  O...O
+    .O...  O...O  OOOO.  .OOO.  OOOO.  OOOOO  O....  .OOO.
+
+    0x48:  0x49:  0x4A:  0x4B:  0x4C:  0x4D:  0x4E:  0x4F:
+    O...O  .OOO.  ..OOO  O...O  O....  O...O  O...O  .OOO.
+    O...O  ..O..  ...O.  O..O.  O....  OO.OO  O...O  O...O
+    O...O  ..O..  ...O.  O.O..  O....  O.O.O  OO..O  O...O
+    OOOOO  ..O..  ...O.  OO...  O....  O.O.O  O.O.O  O...O
+    O...O  ..O..  ...O.  O.O..  O....  O...O  O..OO  O...O
+    O...O  ..O..  O..O.  O..O.  O....  O...O  O...O  O...O
+    O...O  .OOO.  .OO..  O...O  OOOOO  O...O  O...O  .OOO.
+
+    0x50:  0x51:  0x52:  0x53:  0x54:  0x55:  0x56:  0x57:
+    OOOO.  .OOO.  OOOO.  .OOO.  OOOOO  O...O  O...O  O...O
+    O...O  O...O  O...O  O...O  ..O..  O...O  O...O  O...O
+    O...O  O...O  O...O  O....  ..O..  O...O  O...O  O...O
+    OOOO.  O...O  OOOO.  .OOO.  ..O..  O...O  O...O  O...O
+    O....  O.O.O  O.O..  ....O  ..O..  O...O  .O.O.  O.O.O
+    O....  O..O.  O..O.  O...O  ..O..  O...O  .O.O.  OO.OO
+    O....  .OO.O  O...O  .OOO.  ..O..  .OOO.  ..O..  O...O
+
+    0x58:  0x59:  0x5A:  0x5B:  0x5C:  0x5D:  0x5E:  0x5F:
+    O...O  O...O  OOOOO  .OOO.  .....  .OOO.  ..O..  .....
+    O...O  O...O  ....O  .O...  O....  ...O.  .O.O.  .....
+    .O.O.  O...O  ...O.  .O...  .O...  ...O.  .....  .....
+    ..O..  .O.O.  ..O..  .O...  ..O..  ...O.  .....  .....
+    .O.O.  ..O..  .O...  .O...  ...O.  ...O.  .....  .....
+    O...O  ..O..  O....  .O...  ....O  ...O.  .....  .....
+    O...O  ..O..  OOOOO  .OOO.  .....  .OOO.  .....  OOOOO
+
+    0x60:  0x61:  0x62:  0x63:  0x64:  0x65:  0x66:  0x67:
+    ..OO.  .....  O....  .....  ....O  .....  ...OO  .....
+    ..O..  .....  O....  .....  ....O  .....  ..O..  .OO.O
+    ...O.  .OOO.  OOOO.  .OOOO  .OOOO  .OOO.  OOOOO  O..O.
+    .....  ....O  O...O  O....  O...O  O...O  ..O..  O..O.
+    .....  .OOOO  O...O  O....  O...O  OOOOO  ..O..  .OOO.
+    .....  O...O  O...O  O....  O...O  O....  ..O..  ...O.
+    .....  .OOOO  OOOO.  .OOOO  .OOOO  .OOO.  ..O..  OOO..
+
+    0x68:  0x69:  0x6A:  0x6B:  0x6C:  0x6D:  0x6E:  0x6F:
+    O....  .OO..  ..OO.  .O...  .OO..  .....  .....  .....
+    O....  .....  .....  .O...  ..O..  .....  .....  .....
+    O.OO.  .OO..  ..OO.  .O..O  ..O..  OO.O.  O.OO.  .OOO.
+    OO..O  ..O..  ...O.  .O.O.  ..O..  O.O.O  OO..O  O...O
+    O...O  ..O..  ...O.  .OO..  ..O..  O.O.O  O...O  O...O
+    O...O  ..O..  O..O.  .O.O.  ..O..  O.O.O  O...O  O...O
+    O...O  .OOO.  .OO..  .O..O  .OOO.  O.O.O  O...O  .OOO.
+
+    0x70:  0x71:  0x72:  0x73:  0x74:  0x75:  0x76:  0x77:
+    .....  .....  .....  .....  ..O..  .....  .....  .....
+    .....  .....  .....  .....  ..O..  .....  .....  .....
+    OOOO.  .OOOO  .O.OO  .OOOO  .OOOO  O...O  O...O  O...O
+    O...O  O...O  .OO..  O....  ..O..  O...O  O...O  O.O.O
+    O...O  O...O  .O...  .OOO.  ..O..  O...O  .O.O.  O.O.O
+    OOOO.  .OOOO  .O...  ....O  ..O..  O..OO  .O.O.  OO.OO
+    O....  ....O  .O...  OOOO.  ...OO  .OO.O  ..O..  O...O
+
+    0x78:  0x79:  0x7A:  0x7B:  0x7C:  0x7D:  0x7E:  0x7F:
+    .....  .....  .....  ...O.  ..O..  .O...  .OO.O  OOOOO
+    .....  .....  .....  ..O..  ..O..  ..O..  O.OO.  OOOOO
+    O...O  O...O  OOOOO  ..O..  ..O..  ..O..  .....  OOOOO
+    .O.O.  O...O  ...O.  .O...  .....  ...O.  .....  OOOOO
+    ..O..  .OOOO  ..O..  ..O..  ..O..  ..O..  .....  OOOOO
+    .O.O.  ....O  .O...  ..O..  ..O..  ..O..  .....  OOOOO
+    O...O  .OOO.  OOOOO  ...O.  ..O..  .O...  .....  OOOOO
+
+    0x80:  0x81:  0x82:  0x83:  0x84:  0x85:  0x86:  0x87:
+    ...O.  .O...  ...O.  .O...  ...O.  .O...  ...O.  .O...
+    ..O..  ..O..  ..O..  ..O..  ..O..  ..O..  ..O..  ..O..
+    .OOO.  .OOO.  .OOO.  .OOO.  .....  .....  .....  .....
+    ....O  ....O  O...O  O...O  .OO..  .OO..  .OOO.  .OOO.
+    .OOOO  .OOOO  OOOOO  OOOOO  ..O..  ..O..  O...O  O...O
+    O...O  O...O  O....  O....  ..O..  ..O..  O...O  O...O
+    .OOOO  .OOOO  .OOOO  .OOOO  .OOO.  .OOO.  .OOO.  .OOO.
+
+    0x88:  0x89:  0x8A:  0x8B:  0x8C:  0x8D:  0x8E:  0x8F:
+    ...O.  .O...  .OO.O  .OOO.  .OOO.  .OO..  ..O..  O...O
+    ..O..  ..O..  O..O.  O...O  O...O  O..O.  .....  O...O
+    .....  .....  O...O  O....  .OO..  O..O.  .OOO.  O...O
+    O...O  O...O  OO..O  O....  ...O.  O.O..  ..O..  O...O
+    O...O  O...O  O.O.O  O...O  O...O  O..O.  ..O..  O...O
+    O..OO  O..OO  O..OO  .OOO.  .OOO.  O..O.  ..O..  O.O.O
+    .OO.O  .OO.O  O...O  ..O..  ..O..  O.O..  .OOO.  O..O.
+
+    0x90:  0x91:  0x92:  0x93:  0x94:  0x95:  0x96:  0x97:
+    ..O..  .O.O.  ..O..  .O.O.  ..O..  .....  ..O..  .....
+    .O.O.  .....  .O.O.  .....  .O.O.  .O.O.  .O.O.  .O.O.
+    .OOO.  .OOO.  .OOO.  .OOO.  .....  .....  .....  .....
+    ....O  ....O  O...O  O...O  .OO..  .OO..  .OOO.  .OOO.
+    .OOOO  .OOOO  OOOOO  OOOOO  ..O..  ..O..  O...O  O...O
+    O...O  O...O  O....  O....  ..O..  ..O..  O...O  O...O
+    .OOOO  .OOOO  .OOOO  .OOOO  .OOO.  .OOO.  .OOO.  .OOO.
+
+    0x98:  0x99:  0x9A:  0x9B:  0x9C:  0x9D:  0x9E:  0x9F:
+    ..O..  .....  .OO.O  .....  .....  .O.O.  .....  .....
+    .O.O.  .O.O.  O..O.  .....  .OOO.  ..O..  ..O..  O...O
+    .....  .....  .....  .OOOO  O....  .OO.O  .....  .....
+    O...O  O...O  O.OO.  O....  .OO..  O..O.  .OO..  O...O
+    O...O  O...O  OO..O  O....  ...O.  .OOO.  ..O..  O...O
+    O..OO  O..OO  O...O  .OOOO  OOO..  ...O.  ..O..  O.O.O
+    .OO.O  .OO.O  O...O  ..O..  .O...  .OO..  .OOO.  O..O.
+
+    0xA0:  0xA1:  0xA2:  0xA3:  0xA4:  0xA5:  0xA6:  0xA7:
+    .OOO.  .....  .OOO.  OO..O  .O.O.  .O.O.  .O.O.  .O..O
+    ....O  .....  O...O  OO.O.  ..O..  ..O..  ..O..  O..O.
+    .OOOO  .OO.O  O.OOO  ..O..  .OOO.  .OOO.  .....  .....
+    O...O  O..O.  OO..O  .O...  O....  O...O  O.OO.  .OOO.
+    .OOOO  O..O.  O.OOO  O....  O.OOO  OOOOO  OO..O  O...O
+    .....  O.O.O  O...O  OO.OO  O...O  O....  O...O  O...O
+    OOOOO  .O..O  .OOO.  OO.OO  .OOO.  .OOOO  O...O  .OOO.
+
+    0xA8:  0xA9:  0xAA:  0xAB:  0xAC:  0xAD:  0xAE:  0xAF:
+    .....  OOO..  ..OO.  ..O..  .....  .....  .....  .....
+    .....  O....  .O...  .OOOO  ..O..  ..O..  ..O..  ..O..
+    OOOOO  O.OOO  .O...  O.O..  .O...  .OOO.  ...O.  ..O..
+    .O.O.  O.O..  OOOOO  .OOO.  OOOOO  O.O.O  OOOOO  O.O.O
+    .O.O.  OOOOO  .O...  ..O.O  .O...  ..O..  ...O.  .OOO.
+    .O.O.  ..O..  .O...  OOOO.  ..O..  ..O..  ..O..  ..O..
+    .O.OO  ..OOO  O.OOO  ..O..  .....  .....  .....  .....
+
+    0xB0:  0xB1:  0xB2:  0xB3:  0xB4:  0xB5:  0xB6:  0xB7:
+    .OOO.  ..O..  ..OO.  .OOO.  ..O..  ..O..  ...O.  .O..O
+    O...O  .OO..  .O..O  ....O  ..O..  .....  ..O..  O..O.
+    O...O  ..O..  ...O.  .OOO.  OOOOO  .OOO.  .....  .....
+    O...O  ..O..  ..O..  ....O  ..O..  ..O..  O.OO.  O...O
+    .OOO.  ..O..  .OOOO  .OOO.  ..O..  ..O..  OO..O  O...O
+    .....  .....  .....  .....  .....  ..O..  O...O  O..OO
+    OOOOO  .....  .....  .....  OOOOO  .OOO.  O...O  .OO.O
+
+    0xB8:  0xB9:  0xBA:  0xBB:  0xBC:  0xBD:  0xBE:  0xBF:
+    .....  ..O..  .....  ..O..  O....  O....  O....  .OOOO
+    .....  .....  ..O..  .O.O.  O....  O....  .O...  O....
+    O...O  ..O..  .....  ..O..  O..O.  O..O.  O...O  .OOO.
+    O...O  .O...  OOOOO  .....  ..OO.  ..O.O  .O.OO  O...O
+    O..OO  O....  .....  .....  .O.O.  ....O  O.O.O  .OOO.
+    OOO.O  O...O  ..O..  .....  .OOOO  ...O.  ..OOO  ....O
+    O....  .OOO.  .....  .....  ...O.  ..OOO  ....O  OOOO.
+
+    0xC0:  0xC1:  0xC2:  0xC3:  0xC4:  0xC5:  0xC6:  0xC7:
+    ..O..  ..O..  .O...  ...O.  ..O..  ..O..  ...O.  .O...
+    .O...  ...O.  ..O..  ..O..  .O...  ...O.  ..O..  ..O..
+    ..O..  ..O..  OOOOO  OOOOO  .OOO.  .OOO.  .OOO.  .OOO.
+    .O.O.  .O.O.  O....  O....  ..O..  ..O..  O...O  O...O
+    O...O  O...O  OOOO.  OOOO.  ..O..  ..O..  O...O  O...O
+    OOOOO  OOOOO  O....  O....  ..O..  ..O..  O...O  O...O
+    O...O  O...O  OOOOO  OOOOO  .OOO.  .OOO.  .OOO.  .OOO.
+
+    0xC8:  0xC9:  0xCA:  0xCB:  0xCC:  0xCD:  0xCE:  0xCF:
+    ...O.  .O...  .O.O.  .O.O.  .O.O.  .O.O.  OOOO.  O....
+    ..O..  ..O..  ..O..  ..O..  ..O..  ..O..  .O..O  O....
+    O...O  O...O  OOOO.  .OOO.  .OOOO  OOOOO  .O..O  O...O
+    O...O  O...O  O...O  O...O  O....  ...O.  OOO.O  O....
+    O...O  O...O  OOOO.  O....  .OOO.  ..O..  .O..O  O....
+    O...O  O...O  O..O.  O...O  ....O  .O...  .O..O  O....
+    .OOO.  .OOO.  O...O  .OOO.  OOOO.  OOOOO  OOOO.  OOOOO
+
+    0xD0:  0xD1:  0xD2:  0xD3:  0xD4:  0xD5:  0xD6:  0xD7:
+    ..O..  .O.O.  ..O..  .O.O.  ..O..  .O.O.  ..O..  .O.O.
+    .O.O.  .....  .O.O.  .....  .O.O.  .....  .O.O.  .....
+    ..O..  .....  OOOOO  OOOOO  .OOO.  .OOO.  .OOO.  .OOO.
+    .O.O.  .O.O.  O....  O....  ..O..  ..O..  O...O  O...O
+    O...O  O...O  OOOO.  OOOO.  ..O..  ..O..  O...O  O...O
+    OOOOO  OOOOO  O....  O....  ..O..  ..O..  O...O  O...O
+    O...O  O...O  OOOOO  OOOOO  .OOO.  .OOO.  .OOO.  .OOO.
+
+    0xD8:  0xD9:  0xDA:  0xDB:  0xDC:  0xDD:  0xDE:  0xDF:
+    ..O..  .O.O.  .O.O.  .O.O.  .O.O.  .O.O.  ....O  .OO..
+    .O.O.  .....  ..O..  ..O..  ..O..  ..O..  ..OOO  ..O..
+    .....  O...O  .....  .....  .OOO.  .....  ....O  ..O.O
+    O...O  O...O  .O.OO  ..OOO  O....  OOOO.  .OOOO  ..O..
+    O...O  O...O  .OO..  .O...  .OO..  ..O..  O...O  ..O..
+    O...O  O...O  .O...  .O...  ...O.  .O...  O...O  ..O..
+    .OOO.  .OOO.  .O...  ..OOO  OOO..  OOOO.  .OOOO  .OOO.
+
+    0xE0:  0xE1:  0xE2:  0xE3:  0xE4:  0xE5:  0xE6:  0xE7:
+    .OO.O  ..O..  .OOOO  .OOOO  ..O..  ...O.  .OO.O  .OO.O
+    O..O.  .O.O.  O.O..  O.O..  .O.O.  ..O..  O..O.  O..O.
+    ..O..  ..O..  O.O..  O.O..  .....  .....  .OOO.  O.O.O
+    .O.O.  .O.O.  O.OOO  O.OOO  O...O  O...O  O...O  O.O.O
+    O...O  O...O  OOO..  O.O..  .OOOO  .O.O.  O...O  O.O.O
+    OOOOO  OOOOO  O.O..  O.O..  ....O  ..O..  O...O  .O..O
+    O...O  O...O  O.OOO  .OOOO  .OOO.  ..O..  .OOO.  O.OO.
+
+    0xE8:  0xE9:  0xEA:  0xEB:  0xEC:  0xED:  0xEE:  0xEF:
+    O....  O.OO.  ...O.  ...O.  ...O.  ...O.  OOOOO  .O.O.
+    O....  OO..O  ..O..  ..O..  ..O..  ..O..  .....  ..O..
+    OOOO.  O...O  OOOO.  .OOO.  .OOOO  OOOOO  ..O..  .O.O.
+    O...O  O...O  O...O  O...O  O....  ...O.  ..O..  ....O
+    O...O  O...O  OOOO.  O....  .OOO.  ..O..  OOOOO  .OOOO
+    OOOO.  O...O  O..O.  O...O  ....O  .O...  ..O..  O...O
+    O....  O..O.  O...O  .OOO.  OOOO.  OOOOO  ..O..  .OOO.
+
+    0xF0:  0xF1:  0xF2:  0xF3:  0xF4:  0xF5:  0xF6:  0xF7:
+    .OO.O  ..OO.  .....  .....  ..O..  ...O.  .OO.O  .....
+    O..O.  ..OO.  .....  .....  .O.O.  ..O..  O..O.  .....
+    .OOOO  .OOO.  OO.O.  .O.O.  .....  .....  .....  .OO.O
+    ....O  ....O  ..O.O  O.O.O  O.O.O  O...O  .OOO.  O..O.
+    .OOOO  .OOOO  .OOOO  O.OOO  O.O.O  .OOOO  O...O  O.O.O
+    O...O  O...O  O.O..  O.O..  OO.OO  ....O  O...O  .O..O
+    .OOOO  .OOOO  .O.OO  .O.OO  O...O  .OOO.  .OOO.  O.OO.
+
+    0xF8:  0xF9:  0xFA:  0xFB:  0xFC:  0xFD:  0xFE:  0xFF:
+    .....  .O.O.  ...O.  ...O.  ...O.  ...O.  .O...  O.O.O
+    .....  .OO.O  ..O..  ..O..  ..O..  ..O..  OOO..  .O.O.
+    .O...  .O..O  .....  .....  .OOO.  .....  .O..O  O.O.O
+    .OOO.  .O..O  .O.OO  .OOO.  O....  OOOO.  .O.O.  .O.O.
+    .O..O  ....O  .OO..  O....  .OO..  ..O..  .OO..  O.O.O
+    .OOO.  ....O  .O...  O....  ...O.  .O...  .O...  .O.O.
+    .O...  ..OO.  .O...  .OOO.  OOO..  OOOO.  OOOOO  O.O.O
+'''
+
 def _encode_charset(text):
     data = [0] * 7 * 256
     pixels = [ c for c in text if c in ('.', 'O') ]
@@ -593,5 +883,6 @@ def _encode_charset(text):
                     pixel_index += 1
     return tuple(data)
 
-PREMIUM_4 = _encode_charset(_PREMIUM_4)
-PREMIUM_5 = _encode_charset(_PREMIUM_5)
+VW_PREMIUM_4 = _encode_charset(_VW_PREMIUM_4)
+VW_PREMIUM_5 = _encode_charset(_VW_PREMIUM_5)
+SONY_MDX_C7900R = _encode_charset(_SONY_MDX_C7900R)
