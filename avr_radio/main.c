d800955b5fe33eee929607a07caa9b59765cffdb
Mike Naberezny <mike@naberezny.com>
2016-09-06 22:39:35 -0700

    Note missing pin assignments

--- avr_radio/main.c
+++ avr_radio/main.c
@@ -1,12 +1,15 @@
-/* Pin  1: PB0 (not connected)
- * Pin  2: PB1 (not connected)
+/* ...
  * Pin  3: PB2 STB in from radio
  * Pin  4: PB3 /SS out (software generated from STB, connect to PB4)
  * Pin  5: PB4 /SS in (connect to PB3)
  * Pin  6: PB5 MOSI in (to radio's DAT)
  * Pin  7: PB6 MISO out (connect to PB5 through 10K resistor)
  * pin  8: PB7 SCK in from radio
+ * pin  9: /RESET to GND through pushbutton, 10K pullup to Vcc
+ * pin 10: Vcc (connect to unswitched 5V)
  * Pin 11: GND (connect to radio's GND)
+ * Pin 12: XTAL2 (to 20 MHz crystal)
+ * Pin 13: XTAL1 (to 20 MHz crystal)
  * Pin 14: PD0/RXD (to PC's serial TXD)
  * Pin 15: PD1/TXD (to PC's serial RXD)
  * Pin 16: PD2/RXD1 MISO in (from faceplate's DAT)
@@ -15,8 +18,25 @@
  * Pin 19: PD5 Green LED
  * Pin 20: PD6 Red LED
  * Pin 21: PD7 STB out to faceplate
- * Pin 39: PA1 (not connected)
- * Pin 40: PA0 (not connected)
+ * ...
+ * Pin 24: PC2 TCK (to JTAG)
+ * Pin 25: PC3 TMS (to JTAG)
+ * Pin 26: PC4 TDO (to JTAG)
+ * Pin 27: PC5 TDI (to JTAG)
+ * ...
+ * Pin 31: GND
+ * ...
+ *
+ * Pin assignment needed:
+ *  - LOF from radio
+ *  - BUS from faceplate
+ *  - BUS to radio
+ *  - !POWER_EJECT
+ *  - EJECT
+ *
+ * Consider reassigning pins:
+ *  - Move Green LED and Red LED somewhere else so all faceplate signals
+ *    can be grouped together
  */
 
 #include "main.h"
