02bd36d319119f93dc09b6bf10cca3c7fdc6d079
Mike Naberezny <mike@naberezny.com>
2017-05-02 11:09:16 -0700

    Dump memory from a program in RAM

--- submcu/external_rom/rom.asm
+++ submcu/external_rom/rom.asm
@@ -2,30 +2,33 @@
 	.area CODE1 (ABS)
 	.org 0xe000
 
-ddr3 = 0x0d 			;Port 3 Data Direction Register (0=input, 1=output)
+pdr2 = 0x04 			;Port 2 Data Register (port is output only)
 pdr3 = 0x0c 			;Port 3 Data Register
+ddr3 = 0x0d 			;Port 3 Data Direction Register (0=input, 1=output)
+ram = 0x80 				;Start address of RAM
 
 start:
-	;Load RAM with a program to toggle Port 3
+	mov ddr3, #0xff 	;DDR3 = all outputs
 
-	mov 0x80, #0x85 	;0x80 85 0D FF MOV DDR3, 0xFF
-	mov 0x81, #ddr3
-	mov 0x82, #0xff
+	mov a, #0
+    xchw a, ep 			;EP = 0
 
-	mov 0x83, #0x85		;0x83 85 0C 00 MOV PDR3, 0
-	mov 0x84, #pdr3
-	mov 0x85, #0
+	;Load RAM with a program to dump memory out Port 3
+	mov ram+0x00, #0x07		;0080 07 	   mov a, @ep 	;A = data at address EP
+	mov ram+0x01, #0x45 	;0081 45 0C    mov pdr3, a  ;Put it on Port 3
+	mov ram+0x02, #0x0c
+	mov ram+0x03, #0xf7 	;0083 F7       xchw a, ep 	;A=EP
+	mov ram+0x04, #0xc0 	;0084 C0       incw a 		;Increment A, set Z
+	mov ram+0x05, #0xf7 	;0085 F7       xchw a, ep 	;EP=A
+	mov ram+0x06, #0x21  	;0086 21 00 80 jmp 0x80
+	mov ram+0x07, #0x00
+	mov ram+0x08, #0x80
 
-	mov 0x86, #0x85		;0x86 85 0C FF MOV PDR3, 0xFF
-	mov 0x87, #pdr3
-	mov 0x88, #0xff
+	;Jump to the code we just wrote into RAM
+	jmp ram
 
-	mov 0x89, #0x21 	;0x89 21 00 80 JMP 0x0083
-	mov 0x8a, #0
-	mov 0x8b, #0x83
-
-	;Jump to program in RAM
-	jmp 0x0080
+	;Something easy to spot if dumping the external ROM
+	.byte 77, 73, 75, 69 	;"MIKE"
 
 	.org 0xfffd
 	.byte 0x01 			;mode byte
