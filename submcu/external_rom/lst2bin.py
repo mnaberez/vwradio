5071b80f59563f40b5f1d002d53d22977a5fe37b
Mike Naberezny <mike@naberezny.com>
2017-05-04 09:35:03 -0700

    Print error to stderr

--- submcu/external_rom/lst2bin.py
+++ submcu/external_rom/lst2bin.py
@@ -70,7 +70,7 @@ def main(lstfile, binfile):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print(__doc__)
+        sys.stderr.write("%s\n" % __doc__)
         sys.exit(1)
     else:
         lstfile = sys.argv[1]
