ae9836385ca73ff3c0b1de5c4d10ea70f93ac980
Mike Naberezny <mike@naberezny.com>
2017-12-05 10:22:38 -0800

    Remove hardcoded login

--- avr_kwp1281/firmware/main.c
+++ avr_kwp1281/firmware/main.c
@@ -55,6 +55,16 @@
  * Main
  *************************************************************************/
 
+uint16_t bcd_to_bin(uint16_t bcd)
+{
+    uint16_t bin = 0;
+    bin +=  (bcd & 0x000f);
+    bin += ((bcd & 0x00f0) >>  4) * 10;
+    bin += ((bcd & 0x0f00) >>  8) * 100;
+    bin += ((bcd & 0xf000) >> 12) * 1000;
+    return bin;
+}
+
 int main()
 {
     uart_init(UART_DEBUG, 115200);  // debug messages
@@ -65,13 +75,14 @@ int main()
 
     kwp_send_f0_block();
     kwp_receive_block();
+    uint16_t safe_code_bcd = (kwp_rx_buf[3] << 8) + kwp_rx_buf[4];
 
-    uint16_t safe_code = (kwp_rx_buf[3] << 8) + kwp_rx_buf[4];
     uart_puts(UART_DEBUG, (uint8_t*)"SAFE CODE = ");
-    uart_puthex_16(UART_DEBUG, safe_code);
+    uart_puthex_16(UART_DEBUG, safe_code_bcd);
     uart_puts(UART_DEBUG, (uint8_t*)"\n");
 
-    // kwp_send_login_block(0x10e1, 0x01, 0x869f);
+    // uint16_t safe_code_bin = bcd_to_bin(safe_code_bcd);
+    // kwp_send_login_block(safe_code_bin, 0x01, 0x869f);
     // kwp_receive_block();
     //
     // kwp_send_group_reading_block(0x19);
