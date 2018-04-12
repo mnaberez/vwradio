cc886a1854ba8c54891e7018468aef8c5a92283f
Mike Naberezny <mike@naberezny.com>
2018-04-12 16:54:10 -0700

    Allow any demo to be run from the command line

--- avr_radio/host/vwradio/demo.py
+++ avr_radio/host/vwradio/demo.py
@@ -1,12 +1,20 @@
+import datetime
+import sys
 import time
+
 from vwradio import avrclient
 from vwradio import faceplates
 
 
-class Controller(object):
+class Demo(object):
+    ''' Abstract class '''
     def __init__(self, client, faceplate):
         self.client = client
         self.faceplate = faceplate
+
+    def run(self):
+        ''' Abstract method '''
+        raise NotImplementedError()
 
     def clear(self):
         self.client.faceplate_upd_clear_display()
@@ -49,24 +57,31 @@ class Controller(object):
         self.client.faceplate_upd_send_command([0x80 + index] + list(data))
 
 
-class Demonstrator(object):
-    def __init__(self, controller):
-        self.controller = controller
+class ShowCharsetDemo(Demo):
+    '''Show the entire ROM character set on the faceplate'''
 
-    def show_charset(self):
+    def run(self):
         for i in range(0, 0x100, 7):
-            self.controller.write('0x%02X' % i, pos=0)
+            self.write('0x%02X' % i, pos=0)
             data = [0x20] * 7 # 7 chars, default blank spaces
             for j in range(len(data)):
                 code = i + j
                 if code > 0xFF: # past end of charset
                     break
                 data[j] = code
-            self.controller.write_char_codes(data, pos=4)
+            self.write_char_codes(data, pos=4)
             time.sleep(1)
-        self.controller.clear()
+        self.clear()
 
-    def show_rom_charset_comparison(self):
+
+class ComparseCharsetDemo(Demo):
+    '''Compare the ROM-based character set to the dump included in
+    this Python code, which should match it exactly.  A programmable
+    character is set to the character data, then the display will
+    alternate between it and the ROM character.  No changes should
+    be observed during alternation.'''
+
+    def run(self):
         def read_char_data(charset, index):
             start = i * 7
             end = start + 7
@@ -74,53 +89,77 @@ class Demonstrator(object):
 
         for i in range(0x10, 0x100):
             # define character 0 with the rom pattern
-            data = read_char_data(self.controller.faceplate.ROM_CHARSET, i)
-            self.controller.define_char(0, data)
+            data = read_char_data(self.faceplate.ROM_CHARSET, i)
+            self.define_char(0, data)
 
             # first four chars: display character code in hex
-            self.controller.write('0x%02X' % i, pos=0)
+            self.write('0x%02X' % i, pos=0)
 
             # all other chars: alternate between rom and programmable char 0
             for blink in range(6):
                 for code in (0, i):
-                    self.controller.write_char_codes([code]*7, pos=4)
+                    self.write_char_codes([code]*7, pos=4)
                     time.sleep(0.2)
         self.controller.clear()
 
-    def show_keys(self):
-        self.controller.write('    Hit Key')
+
+class ShowKeysDemo(Demo):
+    '''Wait for a key to be pressed on the faceplate and the display it'''
+
+    def run(self):
+        self.write('    Hit Key')
         while True:
-            keys = self.controller.read_keys()
-            names = [ self.controller.faceplate.get_key_name(k) for k in keys ]
+            keys = self.read_keys()
+            names = [ self.faceplate.get_key_name(k) for k in keys ]
             if names:
                 print("%r" % names)
                 msg = names[0][:11].ljust(11)
-                self.controller.write(msg)
+                self.write(msg)
 
-    def clock(self):
-        self.controller.clear()
-        self.controller.write("Time", pos=0)
+
+class ClockDemo(Demo):
+    '''Display the time of day with a blinking hour-minute separator'''
+    def run(self):
+        self.clear()
+        self.write("Time", pos=0)
         while True:
-            clock = time.strftime("%I:%M%p").lower()
+            now = datetime.datetime.now()
+            sep = (':', ' ')[now.second & 1]
+            clock = now.strftime("%I" + sep + "%M%p").lower()
             if clock[0] == '0':
                 clock = ' ' + clock[1:]
-            self.controller.write(clock, pos=4)
-            time.sleep(0.5)
+            self.write(clock, pos=4)
+
+
+_demos_by_name = {'show_charset':    ShowCharsetDemo,
+                  'compare_charset': ComparseCharsetDemo,
+                  'show_keys':       ShowKeysDemo,
+                  'clock':           ClockDemo,
+                 }
+
+
+def usage():
+    exe = sys.argv[0]
+    names = _demos_by_name.keys()
+    sys.stderr.write("Usage: %s <%s>\n" % (exe, '|'.join(names)))
+    sys.exit(1)
 
 
 def main():
+    if len(sys.argv) != 2:
+        usage()
+
+    klass = _demos_by_name.get(sys.argv[1])
+    if klass is None:
+        usage()
+
     client = avrclient.make_client()
     faceplate = faceplates.Premium4()
     try:
         client.set_auto_display_passthru(False)
         client.set_auto_key_passthru(False)
-        controller = Controller(client, faceplate)
-        controller.clear()
-        demo = Demonstrator(controller)
-#        demo.clock()
-#        demo.show_charset()
-#        demo.show_rom_charset_comparison()
-        demo.show_keys()
+        demo = klass(client, faceplate)
+        demo.run()
     except KeyboardInterrupt:
         # if a command is in process, let it finish
         time.sleep(0.1)
