be265279c3185f19b8a142a961429b22be963e34
Mike Naberezny <mike@naberezny.com>
2018-06-14 12:28:57 -0700

    Move radio cracking to its own file

---
+++ kwp1281_tool/firmware/crack.h
@@ -0,0 +1,3 @@
+uint16_t bcd_to_bin(uint16_t bcd);
+void print_hex16(char *label, uint16_t word);
+void crack();
--- kwp1281_tool/firmware/main.c
+++ kwp1281_tool/firmware/main.c
@@ -1,74 +1,12 @@
 #include "main.h"
 #include "uart.h"
 #include "kwp1281.h"
+#include "crack.h"
 #include <string.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
-
-/*************************************************************************
- * Main
- *************************************************************************/
-
-uint16_t bcd_to_bin(uint16_t bcd)
-{
-    uint16_t bin = 0;
-    bin +=  (bcd & 0x000f);
-    bin += ((bcd & 0x00f0) >>  4) * 10;
-    bin += ((bcd & 0x0f00) >>  8) * 100;
-    bin += ((bcd & 0xf000) >> 12) * 1000;
-    return bin;
-}
-
-void print_hex16(char *label, uint16_t word)
-{
-    uart_puts(UART_DEBUG, label);
-    uart_puthex16(UART_DEBUG, word);
-    uart_puts(UART_DEBUG, "\n");
-}
-
-void crack()
-{
-    kwp_result_t result;
-
-    if (memcmp(&kwp_component_1[7], "3CP", 3) == 0) {
-        uart_puts(UART_DEBUG, "CLARION PREMIUM 4 DETECTED\n");
-
-        uint16_t safe_code;
-        result = kwp_p4_read_safe_code_bcd(&safe_code);
-        kwp_panic_if_error(result);
-
-        print_hex16("SAFE Code: ", safe_code);
-
-    } else if (memcmp(&kwp_component_1[7], "DE2", 3) == 0) {
-        uart_puts(UART_DEBUG, "DELCO PREMIUM 5 DETECTED\n");
-        kwp_disconnect();
-        result = kwp_autoconnect(KWP_RADIO_MFG);
-        kwp_panic_if_error(result);
-        result = kwp_p5_login_mfg();
-        kwp_panic_if_error(result);
-
-        uint16_t rom_checksum;
-        result = kwp_p5_calc_rom_checksum(&rom_checksum);
-        kwp_panic_if_error(result);
-
-        uint16_t safe_code;
-        result = kwp_p5_read_safe_code_bcd(&safe_code);
-        kwp_panic_if_error(result);
-
-        print_hex16("ROM Checksum: ", rom_checksum);
-        print_hex16("SAFE Code: ", safe_code);
-
-    } else if (memcmp(&kwp_component_1[7], "YD5", 3) == 0) {
-        uart_puts(UART_DEBUG, "TECHNISAT GAMMA 5 DETECTED\n");
-        uart_puts(UART_DEBUG, "UNCRACKABLE\n");
-
-    } else {
-        uart_puts(UART_DEBUG, "UNKNOWN RADIO\n");
-        uart_puts(UART_DEBUG, "UNCRACKABLE\n");
-    }
-}
 
 
 int main()
