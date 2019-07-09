6cc4b659ddb1d754f5e31b9621dac1ac4960b0aa
Mike Naberezny <mike@naberezny.com>
2019-07-09 09:36:00 -0700

    Only allow building on supported value of F_CPU

--- kwp1281_tool/firmware/autobaud.h
+++ kwp1281_tool/firmware/autobaud.h
@@ -3,6 +3,10 @@
 
 #include "kwp1281.h"
 
+#if F_CPU != 20000000
+#error "Automatic baud rate detection is not supported for this value of F_CPU"
+#endif
+
 kwp_result_t autobaud(uint32_t *actual_baud_rate, uint32_t *normal_baud_rate);
 
 #endif
