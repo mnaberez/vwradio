d4b63712893dad8201f06f4ea86053b4a75a65a1
Mike Naberezny <mike@naberezny.com>
2019-08-22 09:45:13 -0700

    Fix typo in comment

--- kwp1281_tool/firmware/autobaud.h
+++ kwp1281_tool/firmware/autobaud.h
@@ -4,8 +4,8 @@
 #include "kwp1281.h"
 
 #if F_CPU == 20000000
-#define AUTOBAUD_PRESCALER  _BV(CS11)       /* 20 MHz / 8 prescaler = 8 MHz */
-#define AUTOBAUD_PERIOD     0.4             /* 0.4 uS is the period of 8 MHz */
+#define AUTOBAUD_PRESCALER  _BV(CS11)       /* 20 MHz / 8 prescaler = 2.5 MHz */
+#define AUTOBAUD_PERIOD     0.4             /* 0.4 uS is the period of 2.5 MHz */
 #else
 #error "Automatic baud rate detection is not supported for this value of F_CPU"
 #endif
