aa6d3370ab4ecde395739b58532966fdd1a74c4c
Mike Naberezny <mike@naberezny.com>
2020-07-02 09:18:55 -0700

    Revert accidental change to main

--- kwp1281_tool/firmware/main.c
+++ kwp1281_tool/firmware/main.c
@@ -12,26 +12,10 @@ int main(void)
     uart_init(UART_KLINE, 10400);  // obd-ii kwp1281
     sei();
 
-    kwp_result_t result = kwp_retrying_connect(KWP_RADIO_MFG);
+    kwp_result_t result = kwp_retrying_connect(KWP_RADIO);
     kwp_panic_if_error(result);
 
-    result = kwp_p5_login_mfg();
-    kwp_panic_if_error(result);
-
-    uint16_t checksum = 0;
-    result = kwp_p5_calc_rom_checksum(&checksum);
-    kwp_panic_if_error(result);
-    uart_puthex16(UART_DEBUG, checksum);
-
-    result = kwp_read_ram(0, 0xffff, 1);
-    kwp_panic_if_error(result);
-
-    // result = kwp_login_safe(1866);
-    // kwp_panic_if_error(result);
-    // result = kwp_read_ram(0, 0xffff, 32);
-    // kwp_panic_if_error(result);
-
-    // crack();
+    crack();
 
     while(1);
 }
